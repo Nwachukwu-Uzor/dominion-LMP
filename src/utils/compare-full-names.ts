f02239c86963aeb3c81ed29479ded293aadc6606
¨Nwachukwu-Uzor¨ <uzor1997@gmail.com>
2025-04-27 20:36:24 +0100

    update name comparator to allow one name match

--- src/utils/compare-full-names.ts
+++ src/utils/compare-full-names.ts
@@ -1,27 +1,43 @@
+// export function compareFullNames(
+//   fullNameFromIppis: string,
+//   fullNameFromBvn: string,
+// ): boolean {
+//   console.log({ fullNameFromIppis, fullNameFromBvn });
+
+//   const normalize = (name: string) => {
+//     return name
+//       .toLowerCase()
+//       .split(/[ ,]+/) // Split by spaces or commas
+//       .filter((n) => n.trim() !== "") // Remove empty strings
+//       .sort() // Sort to ensure order doesn't matter
+//       .join(" ");
+//   };
+
+//   const normalizedNamesFromIppis = normalize(fullNameFromIppis)
+//     .split(" ")
+//     .filter((name) => name.length > 0)
+//     .map((name) => name.toLowerCase());
+//   const normalizedNamesFromBvn = normalize(fullNameFromBvn)
+//     .split(" ")
+//     .filter((name) => name.trim().length > 0)
+//     .map((name) => name.toLowerCase());
+//   return normalizedNamesFromIppis.every((name) =>
+//     normalizedNamesFromBvn.includes(name),
+//   );
+// }
+
 export function compareFullNames(
   fullNameFromIppis: string,
   fullNameFromBvn: string,
-): boolean {
-  console.log({ fullNameFromIppis, fullNameFromBvn });
+) {
+  const normalizedFullNameFromIppisArray = fullNameFromIppis
+    ?.split(" ")
+    ?.map((name) => name.toLocaleUpperCase());
+  const normalizedFullNameFromBvnArray = fullNameFromBvn
+    ?.split(" ")
+    ?.map((name) => name.toLocaleUpperCase());
 
-  const normalize = (name: string) => {
-    return name
-      .toLowerCase()
-      .split(/[ ,]+/) // Split by spaces or commas
-      .filter((n) => n.trim() !== "") // Remove empty strings
-      .sort() // Sort to ensure order doesn't matter
-      .join(" ");
-  };
-
-  const normalizedNamesFromIppis = normalize(fullNameFromIppis)
-    .split(" ")
-    .filter((name) => name.length > 0)
-    .map((name) => name.toLowerCase());
-  const normalizedNamesFromBvn = normalize(fullNameFromBvn)
-    .split(" ")
-    .filter((name) => name.trim().length > 0)
-    .map((name) => name.toLowerCase());
-  return normalizedNamesFromIppis.every((name) =>
-    normalizedNamesFromBvn.includes(name),
+  return normalizedFullNameFromBvnArray.some((name) =>
+    normalizedFullNameFromIppisArray.includes(name),
   );
 }
